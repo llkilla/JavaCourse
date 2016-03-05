391b5080f937df9e37dd25ff6ea03395b5025209
Софья <ilyina.sophia.s@gmail.com>
2016-03-05 16:42:28 +0600

    Updated output message

--- src/CircleArea.java
+++ src/CircleArea.java
@@ -2,8 +2,8 @@
 public class CircleArea {
 	public static void main(String[] args){
 		double R = Double.parseDouble(args[0]);
-		System.out.println("Circle area= " + CalcCircleArea(R));
-	}
+		System.out.println("With R= " + R + " Circle area= " + CalcCircleArea(R));
+	} 
 	// Процедура вычисления площади круга
 	public static double CalcCircleArea(double R){
 		double Pi = 3.14;
