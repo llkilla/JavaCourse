92f0c2b8d1f56275b33c4d9ea9cd2df4e83e30fd
Софья <ilyina.sophia.s@gmail.com>
2016-03-19 16:55:02 +0600

    Released class Alphabet

---
+++ src/Alphabet.java
@@ -0,0 +1,13 @@
+
+public class Alphabet {
+
+	public static void main(String[] args) {
+		for (int i=(int)'А'; i<(int)'Я'; i++){
+			System.out.print((char)i);
+	}
+		System.out.println();
+	for (int i=(int)'а'; i<(int)'я'; i++){
+		System.out.print((char)i);
+		}
+	}
+}
